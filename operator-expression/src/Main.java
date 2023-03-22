0c555d6fa6c871297b6e24536cd8244f74d975b8
namnh <33839115+namnh43@users.noreply.github.com>
2023-03-22 14:34:41 +0700

    update day of month

--- operator-expression/src/Main.java
+++ operator-expression/src/Main.java
@@ -12,5 +12,35 @@ public class Main {
         height = scanner.nextFloat();
         float area = width * height;
         System.out.println("Area is: "+ area);
+
+        day_of_month();
+    }
+    public static void day_of_month() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Which month do you want to count the day?");
+        int month = scanner.nextInt();
+        switch (month) {
+            case 2:
+                System.out.println("28 or 29 days");
+                break;
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12:
+                System.out.println("31 days");
+                break;
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                System.out.println("30 days");
+                break;
+            default:
+                System.out.println("Not valid");
+                break;
+        }
     }
 }
